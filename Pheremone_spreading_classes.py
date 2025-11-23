9e50e56570510dd2e1f823be960c4cee9babce65
Lakshaya Navaneetham <lakshaya.nav@gmail.com>
2025-11-23 19:23:41 +0000

    Add files via upload

--- Pheremone_spreading_classes.py
+++ Pheremone_spreading_classes.py
@@ -2,6 +2,10 @@ import random
 import pygame
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+from scipy.spatial import KDTree
+
 
 class BeeGroup:
     def __init__(self, name, color, population, radius_range, centre):
@@ -35,9 +39,13 @@ class BeeGroup:
         return ux, uy
 
     # plot the bees on the screen
-    def plot_bees(self, screen):
-        for x, y in self.positions:
-            pygame.draw.circle(screen, self.color, (int(x), int(y)), 2)
+    def plot_bees(self, screen, pheromones, offset):
+        for i, (x,y) in enumerate(self.positions):
+            bee_index = offset + i
+            if pheromones[bee_index] > 0:
+                pygame.draw.circle(screen, (255,255,255), (int(x), int(y)), 2)
+            else:
+                pygame.draw.circle(screen, self.color, (int(x), int(y)), 2)
 
 class Retinue(BeeGroup):
     ATTRACTION_STRENGTH = 0.1
@@ -143,6 +151,59 @@ class Drone(OtherBees):
     ATTRACTION_STRENGTH = 0.002
     NOISE = 1.0
 
+class PheromoneSpreading:
+    def __init__(self, positions):
+        self.queen_lifetime = 3
+        self.alpha = 1.5
+        self.beta = 0.0025
+        self.p_init = 130
+        self.positions = positions
+        self.graph = nx.Graph()
+        self.pheromone_per_bee = [0] * len(self.positions)
+        self.pheromone_per_bee[0] = self.p_init
+        self.transfer_rate = 0.01
+
+    def pheromone_calc(self, t):
+        p = self.p_init*math.exp(-self.alpha*(1-math.exp(-self.beta * t)))
+        return p
+
+    def plot_pheromone_levels(self):
+        days = self.queen_lifetime * 365
+        t = []
+        p = []
+        for i in range(days):
+            t.append(i)
+            p.append(self.pheromone_calc(i))
+
+        plt.plot(t,p)
+        plt.xlabel("Queen age (days)")
+        plt.ylabel("Pheromone level")
+        plt.title("Queen pheromone decline over time")
+        plt.grid(True)
+        plt.show()
+
+    def update_pheromones(self, threshold=3):
+        tree = KDTree(self.positions)
+        pairs = tree.query_pairs(r=threshold)  # only return pairs within threshold
+
+        for i, pos in enumerate(self.positions):
+            self.graph.add_node(i, pos=pos)
+
+        for i, j in pairs:
+            dist = math.dist(self.positions[i], self.positions[j])
+            self.graph.add_edge(i, j, weight=1 / dist)
+
+        updates = [0] * len(self.pheromone_per_bee)
+        for i, p in enumerate(self.pheromone_per_bee):
+            if p > 0:
+                neighbors = list(self.graph.neighbors(i))
+                for n in neighbors:
+                    updates[n] += p * self.transfer_rate * self.graph[i][n]['weight']
+
+        for i in range(len(self.pheromone_per_bee)):
+            self.pheromone_per_bee[i] += updates[i]
+
+
 
 class Simulation:
     def __init__(self, width=1000, height=1000):
@@ -156,10 +217,22 @@ class Simulation:
         self.workers = WorkerBees('in_hive_workers', (0, 255, 255), population['in_hive_workers'], [100, 300], centre)
         self.drones = Drone('drones', (0, 128, 255), population['drones'], [200, 400], centre)
 
+        bees_positions = (self.queen.positions + self.retinues.positions + self.nurses.positions + self.workers.positions + self.drones.positions)
+        self.pheromones = PheromoneSpreading(bees_positions)
 
         pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
+
+    def quit(self):
+        total = 0
+        for i in self.pheromones.pheromone_per_bee:
+            if i > 0:
+                total += 1
+
+        if total > 0.98 * len(self.pheromones.pheromone_per_bee):
+            return True
+        return False
 
     def run(self):
         running = True
@@ -170,11 +243,21 @@ class Simulation:
 
             self.screen.fill((0,0,0))
             queen_pos = self.queen.positions[0]
-
-            self.queen.plot_bees(self.screen)
+            # plot queen bee
+            index = 0
+            self.queen.plot_bees(self.screen, self.pheromones.pheromone_per_bee, index)
+            index += len(self.queen.positions)
             for group in [self.retinues, self.nurses, self.workers, self.drones]:
                 group.update_pos(queen_pos)
-                group.plot_bees(self.screen)
+                group.plot_bees(self.screen, self.pheromones.pheromone_per_bee, index)
+                index += len(group.positions)
+
+            all_positions = (self.queen.positions + self.retinues.positions + self.nurses.positions + self.workers.positions + self.drones.positions)
+            self.pheromones.positions = all_positions
+            self.pheromones.update_pheromones()
+
+            if self.quit():
+                running = False
 
             pygame.display.flip()
             self.clock.tick(60)
@@ -184,5 +267,3 @@ class Simulation:
 if __name__ == '__main__':
     sim = Simulation()
     sim.run()
-
-
